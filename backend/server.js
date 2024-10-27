cf7799a2b10959d3236b1e640bfa9e27bba72dce
jafechi <bgutierrez@dc.uba.ar>
2024-10-27 04:57:26 -0300

    chore: Add .env.sample file

--- backend/server.js
+++ backend/server.js
@@ -16,12 +16,17 @@ dotenv.config();
 // Initialize express app
 const app = express();
 
-// Initialize Sequelize
-const sequelize = new Sequelize('verLaCarta', 'postgres', process.env.DB_PASSWORD, {
-    host: 'localhost',
-    dialect: 'postgres',
-    port: 5432,
-});
+const sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+        host: process.env.DB_HOST,
+        dialect: 'postgres',
+        port: process.env.DB_PORT,
+    }
+);
+
 
 // Test the connection
 try {
