2eb3aa3b1943766bd84fa499a835bb05ef485b74
jafechi <bgutierrez@dc.uba.ar>
2024-11-17 15:18:14 -0300

    feat: Modify Restaurant-related endpoints to use the new '/restaurantes' convention

--- backend/routes/restaurantRoutes.js
+++ backend/routes/restaurantRoutes.js
@@ -6,10 +6,10 @@ const upload = multer();
 export function createRestaurantRouter(restaurantController) {
     const router = Router();
 
-    router.get('/restaurante', restaurantController.getRestaurants.bind(restaurantController));
-    router.get('/restaurante/:id', restaurantController.getRestaurant.bind(restaurantController));
+    router.get('/restaurantes', restaurantController.getRestaurants.bind(restaurantController));
+    router.get('/restaurantes/:id', restaurantController.getRestaurant.bind(restaurantController));
     router.post(
-        '/restaurante',
+        '/restaurantes',
         upload.any(),
         restaurantController.createRestaurant.bind(restaurantController)
     );
