259351029ded07226e8635bab2aea485c7d94a19
jafechi <bgutierrez@dc.uba.ar>
2024-11-21 12:03:06 -0300

    fix: Remove OrderStatusHistory logic

--- backend/infrastructure/database/models/index.js
+++ backend/infrastructure/database/models/index.js
@@ -3,7 +3,6 @@ import Restaurant from './Restaurant.js';
 import MenuItem from './MenuItem.js';
 import OrderStatus from './OrderStatus.js';
 import Order from './Order.js';
-import OrderStatusHistory from '../../../unused_orm_models/OrderStatusHistory.js';
 import OrderItem from './OrderItem.js';
 
 export default function initModels(sequelize) {
@@ -13,7 +12,6 @@ export default function initModels(sequelize) {
         MenuItem: MenuItem(sequelize),
         OrderStatus: OrderStatus(sequelize),
         Order: Order(sequelize),
-        OrderStatusHistory: OrderStatusHistory(sequelize),
         OrderItem: OrderItem(sequelize),
         sequelize: sequelize, // xdd?
     };
@@ -31,12 +29,6 @@ export default function initModels(sequelize) {
 
     models.OrderStatus.hasMany(models.Order, { foreignKey: 'status_id' });
     models.Order.belongsTo(models.OrderStatus, { foreignKey: 'status_id' });
-
-    models.Order.hasMany(models.OrderStatusHistory, { foreignKey: 'order_id' });
-    models.OrderStatusHistory.belongsTo(models.Order, { foreignKey: 'order_id' });
-
-    models.OrderStatus.hasMany(models.OrderStatusHistory, { foreignKey: 'status_id' });
-    models.OrderStatusHistory.belongsTo(models.OrderStatus, { foreignKey: 'status_id' });
 
     models.Order.hasMany(models.OrderItem, { foreignKey: 'order_id' });
     models.OrderItem.belongsTo(models.Order, { foreignKey: 'order_id' });
